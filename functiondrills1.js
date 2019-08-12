eca022b0c1183468e05d643a4216a60f31b0e194
Quasar Wei <quasar.wei@outlook.com>
2019-08-12 11:03:18 -0700

    added arguments to createGreeting()

--- functiondrills1.js
+++ functiondrills1.js
@@ -1,9 +1,8 @@
 'use strict'
 
-function createGreeting() {
-    return `Hi, my name is Chris and I'm 29 years old`;
-
+function createGreeting(name, age) {
+    return `Hi, my name is ${name} and I'm ${age} years old`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('Chris', 29);
 console.log(greeting1);
